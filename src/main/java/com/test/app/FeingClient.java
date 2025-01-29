60d8658bf4b4d13af2e1288147138b2c5c7393de
Cristian Agamez <cagamez@bancolombia.com.co>
2025-01-29 16:30:40 -0500

    second commit

--- src/main/java/com/test/app/FeingClient.java
+++ src/main/java/com/test/app/FeingClient.java
@@ -3,7 +3,7 @@ package com.test.app;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "feing", url = "micro2:8081")
+@FeignClient(name = "feing", url = "${external.service}:8081")
 public interface FeingClient {
 
     @GetMapping("/api/gretting")
